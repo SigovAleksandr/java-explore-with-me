69f3f311bf34cac0de1f9029102367ece65ea2fc
AleksandrSigov <paperrcutt@yandex.ru>
2023-09-27 22:39:51 +0300

    Add EntityScan

--- stats/server/src/main/java/ru/practicum/ewm/stats/server/StatsServerApp.java
+++ stats/server/src/main/java/ru/practicum/ewm/stats/server/StatsServerApp.java
@@ -2,8 +2,10 @@ package ru.practicum.ewm.stats.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("ru.practicum.ewm.stats.model")
 public class StatsServerApp {
     public static void main(String[] args) {
         SpringApplication.run(StatsServerApp.class, args);
--- stats/server/src/main/java/ru/practicum/ewm/stats/server/StatsServerController.java
+++ stats/server/src/main/java/ru/practicum/ewm/stats/server/StatsServerController.java
@@ -1,5 +1,6 @@
 package ru.practicum.ewm.stats.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.stats.dto.EndpointHitDto;
